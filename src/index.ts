1dc59e489707a64f37377553909cc77fd5b99584
Christian Schoeps <christian.schoeps@sportalliance.com>
2021-11-09 21:11:15 +0100

    some changes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import CountDisplay from "./count-display";
 import CountSetter from "./count-set";
 
+console.log('start');
+
 const cd = new CountDisplay;
 const cs = new CountSetter;
 
+  
 cs.set(42);
 
 cd.show();
